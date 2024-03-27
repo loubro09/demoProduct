44bd474ff93f840618465d31ec77ace45f314e18
loubro09 <louis.r.brown@hotmail.com>
2024-03-27 12:32:05 +0100

    create user class contents

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,4 +1,25 @@
 package Model;
 
 public class User {
+    private String userName;
+    private int age;
+    private int length;
+    private int weight;
+
+    public User (String userName, int age, int length, int weight) {
+        this.userName = userName;
+        this.age =  age;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public int getAge() {return age;}
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
