81b590be6be04c6a31a194cdc6c6748fff2090a0
loubro09 <louis.r.brown@hotmail.com>
2024-03-27 12:34:20 +0100

    method ask for user info

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,14 +1,32 @@
 package Controller;
 
+import Model.BeverageType;
+import Model.User;
 import View.MainView;
 import java.util.Scanner;
 
 public class MainController {
     private MainView view;
     private Scanner scanner;
+    private BeverageType[] beverageType;
 
     public MainController() {
         view = new MainView(this);
         scanner = new Scanner(System.in);
+        beverageType = BeverageType.values();
+        enterUserInfo();
+    }
+
+    private void enterUserInfo() {
+        view.printMessage("Enter username: ");
+        String username = scanner.nextLine();
+        view.printMessage("Enter age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+        view.printMessage("Enter weight in kg: ");
+        int weight = Integer.parseInt(scanner.nextLine());
+        view.printMessage("Enter length in cm: ");
+        int length = Integer.parseInt(scanner.nextLine());
+        User user = new User(username,age, length, weight);
+        view.printMessage("Hello " + username + "!\n");
     }
 }
